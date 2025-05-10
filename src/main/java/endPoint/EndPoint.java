5f3c38d079e63f7a31824bbe98b48269aabb3e3b
Mikhail Kamensky <kam3nskym@yandex.ru>
2025-05-11 01:28:55 +0300

    user model and endpoints added

---
+++ src/main/java/endPoint/EndPoint.java
@@ -0,0 +1,9 @@
+package endPoint;
+
+public class EndPoint {
+    public static String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+    public static String CREATE_USER = "api/auth/register";
+    public static String LOGIN_USER = "api/auth/login";
+    public static String GET_OR_UPDATE_USER_DATA = "api/auth/user";
+    public static String GET_USER_ORDERS = "api/orders";
+}
